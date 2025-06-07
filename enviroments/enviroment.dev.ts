74a414df8a88cb92e5e19804bd2f303b2128b4d7
luisesca1989 <luis.pesamatic@gmail.com>
2025-06-07 18:52:22 -0400

    Practica de to-do-list en angular

---
+++ enviroments/enviroment.dev.ts
@@ -0,0 +1,16 @@
+export const enviroment = {
+    id: 'development',
+    production: false,
+    debug: true,
+    offline: false,
+    apps: {
+        default: {
+            apiKey: "AIzaSyAO5SHJsDcX0XE4WEpR2r07pcpLJt9beqo",
+            authDomain: "ambient-develop.firebaseapp.com",
+            projectId: "ambient-develop",
+            storageBucket: "ambient-develop.firebasestorage.app",
+            messagingSenderId: "945826365916",
+            appId: "1:945826365916:web:f75d5b108a206911d10f19"
+        }
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ang-todolist-01';
+  title: string = 'To-Do-List';
+  listArray: string[] = ['Task 1', 'Task 2'];
+  form: FormGroup = new FormGroup ({
+    newTask: new FormControl('')
+  })
+
+  addTask = () => {
+    const task = this.form.value;
+    this.listArray.push(task.newTask)
+  }
+
+  deleteTask = (index: number) => {
+    this.listArray.splice(index,1);
+  }
+
+  moveUpTask = (index: number) => {
+    if (index > 0) {
+      [this.listArray[index], this.listArray[index-1]] = 
+      [this.listArray[index-1], this.listArray[index]]
+    }
+  }
+
+  moveDownTask = (index: number) => {
+    if ( index < this.listArray.length - 1 ) {
+      [this.listArray[index], this.listArray[index+1]] = 
+      [this.listArray[index+1], this.listArray[index]]
+    }
+  }
+
 }
